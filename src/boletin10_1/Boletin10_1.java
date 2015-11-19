3a5bf470bca133379e82cd1a0a09d0473f9571e1
igonzalezcastro <igonzalezcastro@atanasoff21.danielcastelao.org>
2015-11-19 11:39:56 +0100

    Boletin10_1

---
+++ src/boletin10_1/Boletin10_1.java
@@ -0,0 +1,17 @@
+
+package boletin10_1;
+
+/**
+ *
+ * @author igonzalezcastro
+ */
+public class Boletin10_1 {
+
+    public static void main(String[] args) {
+    Numero obx = new Numero();
+//    obx.numeroIntentos();
+    obx.xogar();
+    
+    }
+    
+}
